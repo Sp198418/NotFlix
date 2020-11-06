c7ec96f54f36f27d30d413e8b673f49f6e154baf
Samuel Pierre Louis <samuelpierrelouis10@gmail.com>
2020-11-06 14:34:07 -0500

    NOT Netflix

--- OMDAPI/scripts.js
+++ OMDAPI/scripts.js
@@ -3,49 +3,51 @@ let query = document.getElementById('search').value ;
 let container = document.getElementById('container');
 let page = 1;
 
+
   function search(finder){
       let url = `http://www.omdbapi.com/?apikey=9f075407&s=${finder}&page=${page}`
-      container.innerHTML= ''
-
+      
+      container.innerHTML= ''// location where card is be filled with information 
+    // fetches the data from websites api
       fetch(url)
-
+    // promised response
     .then(res =>  res.json()) 
-    .then(res=> { 
-      let flims = res.Search
-      movies(flims)})
-    .catch(err => {
+    .then(res=> { //object looks like an array or object
+      let films = res.Search
+      movies(films)})
+    .catch(err => {// handles all information I don't expect to get
       console.log(err);
       
 
   })
 }
-// window.onload = search
 
 
-function movies(flims){
-  for(let i =0; i < flims.length; i++) {
+function movies(films){
+   // loop through each item in the res/img
+  for(let i =0; i < films.length; i++) {
   
-        console.log(flims)
-
+        console.log(films)
+       // step creates each element to API requests 
         let card = document.createElement('div');
         let Poster = document.createElement('div');
         let type = document.createElement('div');
         let title = document.createElement('div');
         
-
+         //adding content to elements
         Poster.setAttribute('class', 'poster')
-        Poster.setAttribute("style", `background-image:url('${flims[i].Poster}')`)
-        title.innerHTML = flims[i].Title;
-        type.innerHTML = flims[i].Type;
+        Poster.setAttribute("style", `background-image:url('${films[i].Poster}')`)
+        title.innerHTML = films[i].Title;
+        type.innerHTML = films[i].Type;
 
         
         card.className = 'card'
         
- 
-        if ( flims[i].Poster == "N/A") {
+         // if statement that handles cases where there is no images for searched movies 
+        if ( films[i].Poster == "N/A") {
           Poster.setAttribute("style", `background-image:url('./assets/placeholder.jpg')`)  
   }
-
+       // append data to the website
         container.appendChild(card);
         card.appendChild(Poster);
         card.appendChild(title);
@@ -61,4 +63,19 @@ function movies(flims){
       search(newOutput)
        
     });
-
+        // invoke the button and creating click button to previous search results
+    document.querySelector("#previousButton").addEventListener("click", function() {
+      page--
+      
+      let newOutput = document.getElementById("search").value 
+      search(newOutput)
+       
+    });
+  
+  //invoke the button and creating click button to search for next page of results 
+    document.querySelector("#nextButton").addEventListener("click", function() {
+      page++
+      let newOutput = document.getElementById("search").value 
+      search(newOutput)
+       
+    });
